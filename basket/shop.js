edb889ef9848f4fd3f3242dbb646a244c1109891
daangast <yasakeee@gmail.com>
2022-03-24 17:09:54 +0100

    Step 2: Continuement

--- basket/shop.js
+++ basket/shop.js
@@ -1,6 +1,15 @@
 // Find all shop items
 let baskets = document.querySelectorAll('.add-basket');
 
+let products = [
+    {
+    name: '',
+    tag: '',
+    price: 15,
+    inBasket: 0
+    },
+]
+
 for (let i = 0; i < baskets.length; i++){
     baskets[i].addEventListener('click', () => {
         basketNumbers()
@@ -8,7 +17,10 @@ for (let i = 0; i < baskets.length; i++){
 }
 
 function basketNumbers() {
+    let productNumbers = localStorage.getItem('basketNumbers');
+
+    productNumbers = parseInt(productNumbers);
     localStorage.setItem('basketNumbers', 1);
 }
 
-// Continue from 6:50
+// Continue from 10:40
